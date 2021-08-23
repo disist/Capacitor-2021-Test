5c8c77be7c5dc28ca1aee541470c120eb22a68d4
Dmytro <disist357@gmail.com>
2021-08-24 00:24:17 +0300

    feat(admob): Add admob test integration

--- android/app/src/main/java/io/ionic/starter/MainActivity.java
+++ android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -1,5 +1,14 @@
 package io.ionic.starter;
 
+import android.os.Bundle;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        registerPlugin(com.getcapacitor.community.admob.AdMob.class);
+    }
+
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,50 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
+import { AdMob, RewardAdOptions, AdLoadInfo, RewardAdPluginEvents, AdMobRewardItem } from '@capacitor-community/admob';
+
 import './Home.css';
 
 const Home: React.FC = () => {
+
+  const initAdmob = () => {
+    AdMob.initialize({
+      requestTrackingAuthorization: true,
+      // testingDevices: ['2077ef9a63d2b398840261c8221a0c9b'],
+      initializeForTesting: true,
+    });
+  }
+
+  const prepareAdmobVideo = () => {
+    AdMob.addListener(RewardAdPluginEvents.Loaded, (info: AdLoadInfo) => {
+      // Subscribe prepared rewardVideo
+    });
+  
+    AdMob.addListener(RewardAdPluginEvents.Rewarded, (rewardItem: AdMobRewardItem) => {
+      // Subscribe user rewarded
+      console.log(rewardItem);
+    });
+  
+    const options: RewardAdOptions = {
+      adId: 'appId',
+      isTesting: true
+      // npa: true
+      // ssv: {
+      //   userId: "A user ID to send to your SSV"
+      //   customData: JSON.stringify({ ...MyCustomData })
+      //}
+    };
+
+    AdMob.prepareRewardVideoAd(options);
+  }
+
+  initAdmob();
+  prepareAdmobVideo();
+
+  const showVideo = () => {
+    AdMob.showRewardVideoAd()
+      .then((rewardItem) => console.log('>> rewardItem', rewardItem));
+  }
+
   return (
     <IonPage>
       <IonHeader>
@@ -17,6 +59,7 @@ const Home: React.FC = () => {
           </IonToolbar>
         </IonHeader>
         <ExploreContainer />
+        <button onClick={showVideo}>Show Video</button>
       </IonContent>
     </IonPage>
   );
